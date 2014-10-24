c631c2db391a506edca8a73156a8a16c055ee1ff
melvincabatuan <melvincabatuan@gmail.com>
2014-10-24 12:50:33 +0800

    Hello CPELEC3

---
+++ app/src/main/java/com/cabatuan/helloandroid/Hello.java
@@ -0,0 +1,16 @@
+package com.cabatuan.helloandroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by cobalt on 10/24/14.
+ */
+public class Hello extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hello);
+    }
+}
